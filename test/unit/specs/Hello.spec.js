79c20ec89a99e7c0b34d0cd63a11841eb6e7a50e
Simon Schwartz <simon.schwartz0@gmail.com>
2017-01-21 18:06:01 +1100

    update tests

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Hello from 'src/components/Hello'
 
-describe('Hello.vue', () => {
+describe('ExchangeCalculator.vue', () => {
   it('should render correct contents', () => {
     const vm = new Vue({
       el: document.createElement('div'),
-      render: (h) => h(Hello)
+      render: (h) => h(ExchangeCalculator)
     })
     expect(vm.$el.querySelector('.hello h1').textContent)
       //.to.equal('Welcome to Your Vue.js App')
