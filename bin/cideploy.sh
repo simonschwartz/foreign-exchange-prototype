b3f75e85388edb382fa9d1bc94b465199d229d1c
Simon Schwartz <simon.schwartz0@gmail.com>
2017-01-21 12:11:43 +1100

    Create index.html
    init

---
+++ bin/cideploy.sh
@@ -0,0 +1,11 @@
+!/usr/bin/env bash
+
+# Exit immediately if there is an error
+set -e
+
+# cause a pipeline (for example, curl -s http://sipb.mit.edu/ | grep foo) to produce a failure return code if any command errors not just the last command of the pipeline.
+set -o pipefail
+
+npm install
+npm run build
+cd dist/
